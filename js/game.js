bd0dc0c68b8c133de3adb235dca9b91c70505d15
hudrec <hudrec11@gmail.com>
2018-02-06 19:26:03 -0500

    add to repository changes

--- js/game.js
+++ js/game.js
@@ -171,31 +171,32 @@ var jugador2 = {ruta : 0,
 
 function init(){
     setTimeout(function(){
-        $("body").css('background-image','url("img/esperando.png")')
-
-        $('#auto')[0].width = window.innerWidth;
-        $('#auto')[0].height = window.innerHeight/2;
-        ctx = document.getElementById("auto").getContext("2d");
-
-        $('#auto1')[0].width = window.innerWidth;
-        $('#auto1')[0].height = window.innerHeight/2;
-        ctx1 = document.getElementById("auto1").getContext("2d");
-
-        var nuevoJugador1 = raceCityJugador(ctx, "Ider", "rojo", 50);
-        var nuevoJugador2 = raceCityJugador(ctx1, "Fredy", "rojo", -50);
-        nuevoJugador1.setCompetidores([nuevoJugador2]);
-        nuevoJugador2.setCompetidores([nuevoJugador1]);
-
-        nuevoJugador1.dibujar(19);
-        nuevoJugador2.dibujar(20);
-
-        nuevoJugador1.useKeyboard();
-        nuevoJugador1.jugar();
-
-        nuevoJugador2.useAlternateKeyboard();
-        nuevoJugador2.jugar();
-
         document.body.style.backgroundImage='none';
+        $("body").css('background-image','url("img/esperando.png")');
+
+        // $('#auto')[0].width = window.innerWidth;
+        // $('#auto')[0].height = window.innerHeight/2;
+        // ctx = document.getElementById("auto").getContext("2d");
+        //
+        // $('#auto1')[0].width = window.innerWidth;
+        // $('#auto1')[0].height = window.innerHeight/2;
+        // ctx1 = document.getElementById("auto1").getContext("2d");
+        //
+        // var nuevoJugador1 = raceCityJugador(ctx, "Ider", "rojo", 50);
+        // var nuevoJugador2 = raceCityJugador(ctx1, "Fredy", "rojo", -50);
+        // nuevoJugador1.setCompetidores([nuevoJugador2]);
+        // nuevoJugador2.setCompetidores([nuevoJugador1]);
+        //
+        // nuevoJugador1.dibujar(19);
+        // nuevoJugador2.dibujar(20);
+        //
+        // nuevoJugador1.useKeyboard();
+        // nuevoJugador1.jugar();
+        //
+        // nuevoJugador2.useAlternateKeyboard();
+        // nuevoJugador2.jugar();
+
+
 
         //correr(ctx,jugador1);
         //correr(ctx1,jugador2);
