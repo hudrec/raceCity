bf6f2cfd3e355ecd9e8f5dfb8c75c940d2f6dd7d
Davi Murillo <davimurillo@paspresentfuture.com>
2018-05-12 01:35:45 +0100

    add pilotos

--- js/game.js
+++ js/game.js
@@ -111,7 +111,7 @@ window.connectManager.on("message", function(data){
             }
         }
     }
-    if (data.message.entrar == "jugador"){
+    if (data.message.action == "render_player"){
         $("body").css('background-image','url("img/fondotv.png")');
 
         var chofer = new Image();
@@ -174,31 +174,6 @@ function init(){
         document.body.style.backgroundImage='none';
         $("body").css('background-image','url("img/esperando.png")');
 
-        // $('#auto')[0].width = window.innerWidth;
-        // $('#auto')[0].height = window.innerHeight/2;
-        // ctx = document.getElementById("auto").getContext("2d");
-        //
-        // $('#auto1')[0].width = window.innerWidth;
-        // $('#auto1')[0].height = window.innerHeight/2;
-        // ctx1 = document.getElementById("auto1").getContext("2d");
-        //
-        // var nuevoJugador1 = raceCityJugador(ctx, "Ider", "rojo", 50);
-        // var nuevoJugador2 = raceCityJugador(ctx1, "Fredy", "rojo", -50);
-        // nuevoJugador1.setCompetidores([nuevoJugador2]);
-        // nuevoJugador2.setCompetidores([nuevoJugador1]);
-        //
-        // nuevoJugador1.dibujar(19);
-        // nuevoJugador2.dibujar(20);
-        //
-        // nuevoJugador1.useKeyboard();
-        // nuevoJugador1.jugar();
-        //
-        // nuevoJugador2.useAlternateKeyboard();
-        // nuevoJugador2.jugar();
 
-
-
-        //correr(ctx,jugador1);
-        //correr(ctx1,jugador2);
     },3000)
 }
